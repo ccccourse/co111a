f726f182ad50239d478b05112be12b4127112767
tvghjjhg <114023agf@gmail.com>
2022-12-27 18:14:54 +0800

    20221227

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -6,4 +6,26 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+  @2
+    M=0     
+    @i
+    M=0     
+(LOOP)
+    @i
+    D=M     
+    @0
+    D=D-M   
+    @END
+    D;JGE    
+
+    @1
+    D=M     
+    @2
+    M=D+M   
+    @i
+    M=M+1   
+    @LOOP
+    0;JMP   
+(END)
+    @END
+    0;JMP
